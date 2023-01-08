c7f3383588a0ae3d383e0d15b1a8a761c0c8330d
Sireh-Sanyang <sanyangsireh5@gmail.com>
2023-01-08 14:50:25 +0000

    Set up structures of quiz funtions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,62 +1,87 @@
-const questions =[
-    {
-        question:"What are cat's unable to taste?",
-        option:['Sweetness','Sourness', 'Bitterness'],
-        answer:0
-    },
-    {
-        question:'What is a group of cats called?',
-        options:['Fluffle','Kennel','Clowder'],
-        answer:2
-    },
-    {
-        question:'At what age can cats become pregnant?',
-        option:['Four months of age','Two months of age','Three months of age',],
-        answer:0
-    },
-    {
-        question:"Cat's use their tails for what?",
-        option:['For communication','For balance','For climbing'],
-        answer:1
-    },
-    {
-        question:"What is something unusual about a cat's nose",
-        option:["Each cat's nose is unique.",'The nose is the only smell organ.','They are not sensitive to strong smell.',],
-        answer:0
-    },
-    {
-        question:"Does a cat dream?",
-        option:['No','Yes','Only when they are young'],
-        answer:1
-    },
-    {
-        question:"What is a feral cat?",
-        option:['A cat that lives in wild','A cat that has an owner', "A kitten that is not breastfeed by it's mother"],
-        answer:0
-    },
-    {
-        question:"Do kittens born into the same litter always have the same father?",
-        option:['Yes','Maybe','No'],
-        answer:2
-    },
-    {
-        question:'Cats have how many toes?',
-        option:['12 toes','18 toes','10 toes'],
-        answer:1
-    },
-    {
-        question:"Do black cats bring bad luck?",
-        option:['No','Yes','Some black cats'],
-        answer:0
-    },
-    {
-        question:'What does it mean when a cat blinks slowly?',
-        option:['They are angry at you','They are hungry','They are kissing you'],
-        answer:2
-    },
-    {
-        question:'Cats spend how much time grooming each day?',
-        option:['1/3 of the day','Halve of the day','Quarter of the day'],
-        answer:0
-    }
-    ]
+const questions = [
+  {
+    question: "What are cat's unable to taste?",
+    option: ["Sweetness", "Sourness", "Bitterness"],
+    answer: 0,
+  },
+  {
+    question: "What is a group of cats called?",
+    options: ["Fluffle", "Kennel", "Clowder"],
+    answer: 2,
+  },
+  {
+    question: "At what age can cats become pregnant?",
+    option: ["Four months of age", "Two months of age", "Three months of age"],
+    answer: 0,
+  },
+  {
+    question: "Cat's use their tails for what?",
+    option: ["For communication", "For balance", "For climbing"],
+    answer: 1,
+  },
+  {
+    question: "What is something unusual about a cat's nose",
+    option: [
+      "Each cat's nose is unique.",
+      "The nose is the only smell organ.",
+      "They are not sensitive to strong smell.",
+    ],
+    answer: 0,
+  },
+  {
+    question: "Does a cat dream?",
+    option: ["No", "Yes", "Only when they are young"],
+    answer: 1,
+  },
+  {
+    question: "What is a feral cat?",
+    option: [
+      "A cat that lives in wild",
+      "A cat that has an owner",
+      "A kitten that is not breastfeed by it's mother",
+    ],
+    answer: 0,
+  },
+  {
+    question:
+      "Do kittens born into the same litter always have the same father?",
+    option: ["Yes", "Maybe", "No"],
+    answer: 2,
+  },
+  {
+    question: "Cats have how many toes?",
+    option: ["12 toes", "18 toes", "10 toes"],
+    answer: 1,
+  },
+  {
+    question: "Do black cats bring bad luck?",
+    option: ["No", "Yes", "Some black cats"],
+    answer: 0,
+  },
+  {
+    question: "What does it mean when a cat blinks slowly?",
+    option: [
+      "They are angry at you",
+      "They are hungry",
+      "They are kissing you",
+    ],
+    answer: 2,
+  },
+  {
+    question: "Cats spend how much time grooming each day?",
+    option: ["1/3 of the day", "Halve of the day", "Quarter of the day"],
+    answer: 0,
+  },
+];
+function getUserName() {}
+function setQuestions() {}
+function getNextQuestion() {}
+function getEffects() {}
+function getanswerIndicator() {}
+function updateAnswerIndicator() {}
+function startQuiz() {}
+function next() {}
+function scores(){}
+function quizOver() {}
+function resetGame() {}
+function goToHomebox() {}
